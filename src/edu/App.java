9624fabfb8e6bd51488c9fca797f4b9893153739
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2024-06-26 17:44:52 -0300

    revisando conteudos

--- src/edu/App.java
+++ src/edu/App.java
@@ -1,7 +1,15 @@
 package edu;
 
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Date dataNascimento = new Date();
+        System.out.println(dataNascimento);
+
+        boolean teste = true;
+
     }
 }
