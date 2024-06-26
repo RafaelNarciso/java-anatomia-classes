cc7bd20813712698dc15f4259c5ec529b8ffcc21
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2024-06-26 18:12:14 -0300

    test operador

---
+++ src/edu/rafael/segundasemana/operadorTernario.java
@@ -0,0 +1,14 @@
+package edu.rafael.segundasemana;
+
+public class operadorTernario {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "true" : "false";
+        System.out.println(resultado);
+
+    }
+
+}
